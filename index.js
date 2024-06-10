bcb3d526a8a56d67d8b7c0bf5fd3ddfa6ac91aaf
piyush4596 <piyushprajapati3008@gmail.com>
2024-06-09 23:04:31 -0400

    having in backend on show.ejs id undefined

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
+    id: uuidv4(),
     username: "devloper",
     content: "making website",
   },
   {
+    id: uuidv4(),
     username: "Piyush",
     content: "finding job",
   },
   {
+    id: uuidv4(),
     username: "Akash",
     content: "java devloper",
   },
@@ -37,6 +41,13 @@ app.post("/posts", (req, res) => {
   let { username, content } = req.body;
   posts.push({ username, content });
   res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
+  console.log(post);
 });
 
 app.listen(port, () => {
