b1f3fe2bfef8ad502aae3d7079ae3cb820cdfef7
piyush4596 <piyushprajapati3008@gmail.com>
2024-06-15 12:57:05 -0400

    finished

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -53,6 +56,26 @@ app.get("/posts/:id", (req, res) => {
   }
   res.render("show.ejs", { post });
   console.log(post);
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
